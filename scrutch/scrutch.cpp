6f5357734062f3cd265eacff88993aa42e88d965
jhuni45 <jhunier12@gmail.com>
2015-06-30 08:56:46 -0500

    :D

--- scrutch/scrutch.cpp
+++ scrutch/scrutch.cpp
@@ -6,7 +6,7 @@ scrutch::scrutch(QWidget *parent) :
     ui(new Ui::scrutch)
 {
     ui->setupUi(this);
-    QPixmap scrutch("C:/Users/toshiba/Desktop/CsOAD-scrUtch-millenium-/scrutch1.png");
+    QPixmap scrutch("C:/Users/toshiba/Desktop/CsOAD-scrUtch-millenium-/build-scrutch-Desktop_Qt_5_4_1_MinGW_32bit-Debug/release/gato.png");
     ui->label->setPixmap(scrutch);
 }
 
